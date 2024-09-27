8c9cd5a349ab46665cfb200a09e9fa6363f31a5d
ias myn <iasminlopes71@gmail.com>
2024-09-26 22:33:29 -0300

    Add files via upload

---
+++ atv1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int anoAtual;
+    int anoNascimento;
+    int idade;
+
+    anoAtual = 2024; 
+    anoNascimento = 2005;
+    idade = anoAtual - anoNascimento;
+
+    printf("A aluna Iasmyn tem %d anos", idade);
+
+    return 0;
+}
+
